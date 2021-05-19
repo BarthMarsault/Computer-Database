eae6d4fda5d785e5e6864593dc243be2af61fe0e
Barth <barth2803@gmail.com>
2021-05-19 11:08:37 +0200

    WIP Lecture des fichiers et création bdd temporaire

--- src/test/java/com/excilys/cdb/TestCompanyDAO.java
+++ src/test/java/com/excilys/cdb/TestCompanyDAO.java
@@ -7,19 +7,12 @@ import static org.mockito.Mockito.when;
 import java.io.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
-import javax.sql.DataSource;
 
 import org.junit.*;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
-import org.mockito.internal.util.reflection.Fields;
 
 import com.excilys.cdb.persistence.DB;
 
@@ -35,22 +28,45 @@ public class TestCompanyDAO {
 	@Before
 	public void init() throws SQLException, IOException {
 		
-	    /*conn = DriverManager.getConnection("jdbc:h2:mem:myDb;DB_CLOSE_DELAY=-1","sa","");
+		String reqCreateDatabase = "";
+		String reqEntries = "";
+		String line;
+		BufferedReader reader = new BufferedReader(new FileReader("src/test/resources/1-SCHEMA.sql"));
+		
+		while ((line = reader.readLine()) != null)
+			reqCreateDatabase += line;
+		reader.close();
+		
+		line = "";
+		reader = new BufferedReader(new FileReader("src/test/resources/3-ENTRIES.sql"));
+		while ((line = reader.readLine()) != null)
+			reqEntries += line;
+		
+		
+		
+	    conn = DriverManager.getConnection("jdbc:h2:mem:myDb;DB_CLOSE_DELAY=-1","sa","");
 	    
 	    Statement stmt = conn.createStatement();
 	    
 	    try {
-	    	stmt.execute("CREATE TABLE test(id int, nom varchar(255))");
-	    	stmt.execute("INSERT INTO test VALUES(1,'NomDeTest')");
+	    	stmt.execute(reqCreateDatabase);
+	    	stmt.execute(reqEntries);
 	    }catch(Exception e){
 	    	System.out.println(e.getMessage());
 	    }
 	    
-		MockitoAnnotations.initMocks(this);
-	    */
+		//MockitoAnnotations.initMocks(this);
 	    
-	    
+		
     }
+	
+	@After
+	public void close() {
+		try {
+			conn.close();
+		} catch (SQLException e) {
+		}
+	}
 	
 	@Test
 	public void testFindCompanyById() {
