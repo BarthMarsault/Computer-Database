92070dbbc29c32b7b93e79f516ed6bfa25dc2816
Barth <barth2803@gmail.com>
2021-05-25 15:47:32 +0200

    Passage des paramètres de DB en db.properties

--- src/main/java/com/excilys/cdb/persistence/DB.java
+++ src/main/java/com/excilys/cdb/persistence/DB.java
@@ -1,6 +1,10 @@
 package com.excilys.cdb.persistence;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.*;
+import java.util.Properties;
+
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,40 +13,62 @@ public class DB {
 	
 	private static final Logger logger = LoggerFactory.getLogger(DB.class);
 	
-	private static DB db = null;
+	private static DB database = null;
 	private Connection conn = null;
 	
-	private String protocole = "jdbc:mysql:";
-	private String ip =  "localhost" ; 
-    private String port =  "3306" ;
-	private String dbName = "computer-database-db";
-	private String user = "admincdb";
-	private String password = "qwerty1234";
+	private final String CONFIG_FILE = "db.properties";
+	private final String DATABASE_DRIVER = "driverClassName";
+	private final String DATABASE_URL = "url";	
+	private final String DATABASE_USER = "username";
+	private final String DATABASE_PASSWORD = "password";
 	
+	private String driver;
+	private String url;
+	private String user;
+	private String pass;
+	
+	
+	private DB() {
+		super();
+		Properties properties = getProperties();
+		driver = properties.getProperty(DATABASE_DRIVER);
+		url = properties.getProperty(DATABASE_URL);
+		user = properties.getProperty(DATABASE_USER);
+		pass = properties.getProperty(DATABASE_PASSWORD);
+	}
 	
 	public static DB getInstance() {
-		if(db == null) {
-			db = new DB();
+		if(database == null) {
+			database = new DB();
 		}
-		return db;
+		return database;
 	}
 	
 	public Connection getConnection() {
 		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			if(conn == null || conn.isClosed()) {       
-		        String connString = protocole +  "//" + ip +  ":" + port +  "/" + dbName ;
-			    conn = DriverManager.getConnection(connString, user, password);
+			Class.forName(driver);
+			if(conn == null || conn.isClosed()) { 
+			    conn =  DriverManager.getConnection(database.url, database.user, database.pass);
 			}
 					    
 		} catch (SQLException | ClassNotFoundException e) {
 			logger.error(e.getMessage());
 		}
 		
-		
 		return conn;
 	}
 	
+	private Properties getProperties() {
+		Properties properties = new Properties();
+		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+		try (InputStream configFile = classLoader.getResourceAsStream(CONFIG_FILE)) {
+			properties.load(configFile);
+		} catch (IOException e) {
+			logger.error(e.getMessage());
+			//e.printStackTrace();
+		}
+		return properties;
+	}
 	
 	public boolean closeConnection() {
 		try {
