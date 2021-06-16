ef76a53e31b97e51474b33829691da495e137d34
Barth <barth2803@gmail.com>
2021-06-16 11:14:59 +0200

    WIP hibernate

--- src/main/java/com/excilys/cdb/Main.java
+++ src/main/java/com/excilys/cdb/Main.java
@@ -2,8 +2,6 @@ package com.excilys.cdb;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,7 +14,7 @@ public class Main {
 	private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
 	public static void main(String[] args) {
-		ApplicationContext context = new AnnotationConfigApplicationContext(Main.class);
+		//ApplicationContext context = new AnnotationConfigApplicationContext(Main.class);
 		
 		
 		//System.out.println(computerDAO.getAll().toString());
